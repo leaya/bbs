bc47683d0a38b6d86102c9b6a4d59322aeb396e1
lihaoxin <331926815@qq.com>
2017-11-14 02:52:07 +0000

    finish basic page layout

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="csrf-token" content="{{csrf_token()}}">
+    <title>@yield ('title', 'leaya BBS') - 中阶论坛</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+<body>
+    <div id="app" class="{{ route_class() }}-page">
+        @include('layouts._header')
+        <div class="container">
+            @yield('content')
+        </div>
+        @include('layouts._footer')
+    </div>
+
+    <script src="{{ asset('js/app.js') }}"></script>
+</body>
+</html>
