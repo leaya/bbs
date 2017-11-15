a9e0f2b82a676889bcdf7e7903fd6c861fac4544
lihaoxin <331926815@qq.com>
2017-11-15 04:08:45 +0000

    add authorized user

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -10,6 +10,11 @@ use Auth;
 
 class UsersController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth')->except('show');
+    }
+
     public function show(User $user)
     {
         return view('users.show', compact('user'));
@@ -17,11 +22,13 @@ class UsersController extends Controller
 
     public function edit(User $user)
     {
+        $this->authorize('update', $user);
         return view('users.edit', compact('user'));
     }
 
     public function update(UserRequest $request, User $user)
     {
+        $this->authorize('update', $user);
         $user->update($request->all());
         return redirect()->route('users.show', $user->id)->with('success', '个人资料更新成功！');
     }
