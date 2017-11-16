723f3be0ac14b3010aab3c8a91c84e628818938c
lihaoxin <331926815@qq.com>
2017-11-16 07:36:06 +0000

    auto translate slug

--- resources/views/topics/show.blade.php
+++ resources/views/topics/show.blade.php
@@ -41,15 +41,21 @@
                     {!! $topic->body !!}
                 </div>
 
+                @can('update', $topic)
                 <div class="operate">
                     <hr>
                     <a href="{{ route('topics.edit', $topic->id) }}" class="btn btn-default btn-xs" role="button">
                         <i class="glyphicon glyphicon-edit"></i> 编辑
                     </a>
-                    <a href="#" class="btn btn-default btn-xs" role="button">
-                        <i class="glyphicon glyphicon-trash"></i> 删除
-                    </a>
+                    <form action="{{ route('topics.destroy', $topic->id) }}" method="POST">
+                        {{ csrf_field() }}
+                        {{ method_field('DELETE') }}
+                        <button type="submit" class="btn btn-default btn-xs" role="button">
+                            <i class="glyphicon glyphicon-trash"></i> 删除
+                        </button>
+                    </form>
                 </div>
+                @endcan
 
             </div>
         </div>
--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,8 @@ Auth::routes();
 Route::resource('users', 'UsersController')->only(['show', 'update', 'edit']);
 
 Route::post('avatar', 'UsersController@changeAvatar');
-Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'create', 'store', 'update', 'edit', 'destroy']]);
+Route::get('topics/{topic}/{slug?}', 'TopicsController@show')->name('topics.show');
 Route::post('upload_image', 'TopicsController@uploadImage')->name('topics.upload_image');
 
 Route::resource('categories', 'CategoriesController')->only(['show']);
